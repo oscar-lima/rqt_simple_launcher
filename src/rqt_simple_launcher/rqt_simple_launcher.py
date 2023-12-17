5176c72e31d907033a05dda4fed97e354d57cd53
Oscar Lima <oscar.lima@dfki.de>
2023-12-17 12:08:46 +0100

    refactor code to cope with two instances of gui plugin on the same panel
    - once the first process finishes, sends a trigger to the second to run,
    this is done via topics.
    - add example perspective config file.
    - added group box to disable widgets when process is either launching or
    running.

--- src/rqt_simple_launcher/rqt_simple_launcher.py
+++ src/rqt_simple_launcher/rqt_simple_launcher.py
@@ -12,6 +12,7 @@ import yaml
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget, QFileDialog
+from std_msgs.msg import String
 
 from PyQt5.QtWidgets import QApplication
 
@@ -68,35 +69,58 @@ class RqtSimpleLauncher(Plugin):
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
 
+        # used when running multiple instances of this plugin as panels within rqt_gui
+        middle_ns = rospy.get_param(f'~args_config_{context.serial_number()}', None)
+        if middle_ns is None:
+            middle_ns = ''
+        else:
+            middle_ns = middle_ns + '/'
+
         # class variables
-        self.launch_on = False
-        self.launch_process = None
+        self.exec_on = False
+        self.exec_process = None
 
         # parameters
         # False: really launches the launch file, True: only prints the command that will be launched for debug purposes
-        self.only_print_cmd = rospy.get_param('~only_print_cmd', False)
-        brief = rospy.get_param('~brief', 'Bringup sim')
-        self.launch = rospy.get_param('~launch', {})
-        self.cmd_args = []
-        self.bool_arg = rospy.get_param('~bool_arg', {})
-        self.args_dictionary = rospy.get_param('~args', {})
-        required_services = rospy.get_param('~required_services', [])
-        self.robot_namespace = rospy.get_param('~robot_namespace', 'mobipick')
-        placeholder_values = rospy.get_param('~placeholder_values', {'robot_namespace': self.robot_namespace})
+        self.only_print_cmd = rospy.get_param(f'~{middle_ns}only_print_cmd', False)
+        brief = rospy.get_param(f'~{middle_ns}brief', 'Bringup sim')
+        self.is_launch_file = True
+        self.launch = rospy.get_param(f'~{middle_ns}launch', {})
+        if self.launch == {}:
+            self.is_launch_file = False
+        self.is_ros_node = True
+        self.run = rospy.get_param(f'~{middle_ns}run', {})
+        if self.run == {}:
+            self.is_ros_node = False
+        else:
+            self._widget.chkBoolParam.hide()
+        # sanity check, either run or launch param must be set
+        if self.launch == {} and self.run == {}:
+            rospy.logerr('params not correctly configured (both launch and run params are missing), will exit')
+            sys.exit(0)
+        # sanity check, can't be launch file and ros node at the same time
+        assert(self.is_launch_file ^ self.is_ros_node)  # XOR
+        self.bool_arg = rospy.get_param(f'~{middle_ns}bool_arg', {})
+        self.args_dictionary = rospy.get_param(f'~{middle_ns}args', {})
+        required_services = rospy.get_param(f'~{middle_ns}required_services', [])
+        self.robot_namespace = rospy.get_param(f'~{middle_ns}robot_namespace', 'mobipick')
+        placeholder_values = rospy.get_param(f'~{middle_ns}placeholder_values', {'robot_namespace': self.robot_namespace})
         self.required_services = self.replace_placeholders(required_services, placeholder_values)
+        self.execution_timeout = rospy.get_param(f'~{middle_ns}execution_timeout', 60)
 
         # construct launch file path
-        if self.launch == {}:
-            rospy.logerr('Required parameter launch not set, exiting')
-            sys.exit(0)
-        else:
-            pkg_path = rospkg.RosPack().get_path(self.launch['ros_pkg'])
-            folder = self.launch['folder']
-            launch_file = self.launch['launch_file']
-            self.launch_file_path = f'{pkg_path}/{folder}/{launch_file}'
+        if self.is_launch_file:
+            if self.launch == {}:
+                rospy.logerr('Required parameter launch not set, exiting')
+                sys.exit(0)
+            else:
+                pkg_path = rospkg.RosPack().get_path(self.launch['ros_pkg'])
+                folder = self.launch['folder']
+                launch_file = self.launch['launch_file']
+                self.launch_file_path = f'{pkg_path}/{folder}/{launch_file}'
 
         # set label of what the button launch will do
-        self._widget.lblBrief.setText(brief)
+        self._widget.groupButtons.setTitle(brief)
 
         # handle bool param, if required set its text, otherwise hide the widget
         if self.bool_arg == {}:
@@ -128,11 +152,18 @@ class RqtSimpleLauncher(Plugin):
             self.comboArgList[i].setCurrentIndex(index)
 
         # make button orange at startup
-        self.configure_launch_button('orange', 'Down\nclick to\nLaunch')
+        self.configure_exec_button_down()
+
+        self._widget.groupArguments.setEnabled(True)
 
         ## make a connection between the qt objects and this class methods
-        self._widget.cmdLaunch.clicked.connect(self.cmd_launch_clicked)
+        self._widget.cmdLaunch.clicked.connect(self.cmd_exec_clicked)
         self._widget.cmdSaveConfig.clicked.connect(self.cmd_save_config_clicked)
+
+        # to trigger the execution of a sibling process (other instances of rqt_simple_launcher) via topic
+        self.trigger_pub = rospy.Publisher(f'~{middle_ns}exec_sibling_process', String, queue_size=1)
+        # external trigger of executing a process via topic
+        rospy.Subscriber(f'~{middle_ns}execute_process', String, self.execute_process)
 
         context.add_widget(self._widget)
 
@@ -156,9 +187,34 @@ class RqtSimpleLauncher(Plugin):
         '''
         Destructor: perform cleanup
         '''
-        self.kill_launch_process()
+        self.kill_exec_process()
         rospy.loginfo('bye bye!')
         sys.exit(0)
+
+    def execute_process(self, msg):
+        '''
+        Callback function to execute a process via topic
+        '''
+        rospy.logwarn('received trigger to exec process via topic')
+        self.cmd_exec_clicked()
+
+    def configure_exec_button_down(self):
+        button_common_text = f'Down\nclick to\n'
+        if self.is_launch_file:
+            button_text = f'{button_common_text}Launch'
+        elif self.is_ros_node:
+            button_text = f'{button_common_text}Run'
+        else:
+            button_text = 'error'
+        self.configure_exec_button('orange', button_text)
+
+    def loginfo_green(self, message):
+        '''
+        This function acts as a wrapper around rospy.loginfo to print messages in green color.
+        '''
+        green_start = '\033[92m'
+        color_reset = '\033[0m'
+        rospy.loginfo(green_start + str(message) + color_reset)
 
     def replace_placeholders(self, item, placeholder_values):
         if isinstance(item, dict):
@@ -170,27 +226,38 @@ class RqtSimpleLauncher(Plugin):
             return item.format(**placeholder_values)
         return item
 
-    def cmd_launch_clicked(self):
-        self.launch_on = not self.launch_on
-        if self.launch_on:
-            self.launch_file_process()
+    def cmd_exec_clicked(self):
+        self.exec_on = not self.exec_on
+        if self.exec_on:
+            if self.is_launch_file:
+                self.launch_file_process()
+            elif self.is_ros_node:
+                self.run_node()
+            else:
+                rospy.logerr('malformed yaml config file (either launch or run param is missing), will exit')
+                sys.exit(0)
         else:
-            self.kill_launch_process()
+            self.kill_exec_process()
 
-    def configure_launch_button(self, color, text):
+    def configure_exec_button(self, color, text):
         self._widget.cmdLaunch.setText(f'State: {text}')
         self._widget.cmdLaunch.setStyleSheet(f'background-color: {color}; color: white;')
         QApplication.processEvents()
 
-    def fill_args(self):
-        self.cmd_args = []
-        bool_arg_value = False
-        if self._widget.chkBoolParam.isChecked():
-            bool_arg_value = True
-        self.cmd_args.append(f"{self.bool_arg['arg']}:={bool_arg_value}")
-        for i, comboArg in enumerate(self.comboArgList):
-            if comboArg.isVisible():
-                self.cmd_args.append(f"{self.args_dictionary[i]['arg']}:={comboArg.currentText()}")
+    def make_launch_args(self):
+        cmd_args = []
+        cmd_args.append(f"{self.bool_arg['arg']}:={self._widget.chkBoolParam.isChecked()}")
+        cmd_args.extend([f"{self.args_dictionary[i]['arg']}:={comboArg.currentText()}"
+                         for i, comboArg in enumerate(self.comboArgList) if comboArg.isVisible()])
+        return cmd_args
+
+    def make_run_args(self):
+        # TODO: not sure what to do with boolean arg for ros nodes...
+        # bool_arg_value = False
+        # if self._widget.chkBoolParam.isChecked():
+            # bool_arg_value = True
+        # cmd_args.append(f"{self.bool_arg['arg']}:={bool_arg_value}")
+        return [comboArg.currentText() for comboArg in self.comboArgList if comboArg.isVisible()]
 
     def check_cmd(self, cmd):
         # prevent shell injection
@@ -208,44 +275,80 @@ class RqtSimpleLauncher(Plugin):
             return False
 
     def wait_for_services(self, required_services, timeout=60):
+        '''
+        Function to wait for required services to be available
+        if timeout occurs then kills the process
+        also handles enable / disable of the arguments group and sets appropriate button text and color
+        '''
+        if self.only_print_cmd:
+            rospy.loginfo('Not waiting for services as only_print_cmd is True')
+            return True
         start_time = time.time()
         while time.time() - start_time < timeout:
             if all(self.service_exists(service) for service in required_services):
                 rospy.loginfo('All services are available!')
-                return True
+                self.configure_exec_button('green', 'Running\nclick to\ntake down')
+                self._widget.groupArguments.setEnabled(False)
+                return
+            QApplication.processEvents() # prevent the system thinking that the application is not responding
             time.sleep(1)
-        rospy.logwarn('Timeout: Not all services were available.')
-        return False
+        rospy.logerr('==============================================================')
+        rospy.logerr(f'\nTimeout: Required services were unavailable after {timeout} s. Killing process :\n')
+        rospy.logerr(f'{self.launch_file_path}')
+        rospy.logerr('==============================================================')
+        self.kill_exec_process()
+        self._widget.groupArguments.setEnabled(True)
 
     def pretty_print_cmd(self, cmd):
         return ' '.join(cmd)
 
     def launch_file_process(self):
-        self.configure_launch_button('red', 'Launching\nplease wait...')
+        self.configure_exec_button('red', 'Launching\nplease wait...')
+        self._widget.groupArguments.setEnabled(False)
         self.check_cmd(self.launch_file_path)
-        self.fill_args()
-        for arg in self.cmd_args:
+        cmd_args = self.make_launch_args()
+        for arg in cmd_args:
             self.check_cmd(arg)
-        cmd = ['roslaunch', self.launch_file_path] + self.cmd_args
-        rospy.loginfo(f'Launching:\n{self.pretty_print_cmd(cmd)}')
+        cmd = ['roslaunch', self.launch_file_path] + ['__ns:=/'] + cmd_args
+        rospy.loginfo(f'Launching:')
+        self.loginfo_green(self.pretty_print_cmd(cmd))
         if not self.only_print_cmd:
             # shell=False is important as it is safer
-            self.launch_process = subprocess.Popen(cmd, shell=False, preexec_fn=os.setsid)
-        self.wait_for_services(self.required_services)
-        self.configure_launch_button('green', 'Running\nclick to\ntake down')
+            self.exec_process = subprocess.Popen(cmd, shell=False, preexec_fn=os.setsid)
+        self.wait_for_services(self.required_services, timeout=self.execution_timeout)
+        self.trigger_pub.publish(String('done'))
 
-    def kill_launch_process(self):
+    def run_node(self):
+        self.configure_exec_button('red', 'Running\nplease wait...')
+        self._widget.groupArguments.setEnabled(False)
+        ros_pkg = self.run['ros_pkg']
+        self.check_cmd(ros_pkg)
+        cmd_args = self.make_run_args()
+        for arg in cmd_args:
+            self.check_cmd(arg)
+        cmd = ['rosrun', ros_pkg, self.run['ros_node']] + ['__ns:=/'] + cmd_args
+        rospy.loginfo(f'Running:')
+        self.loginfo_green(self.pretty_print_cmd(cmd))
+        if not self.only_print_cmd:
+            # shell=False is important as it is safer
+            self.exec_process = subprocess.Popen(cmd, shell=False, preexec_fn=os.setsid)
+        self.wait_for_services(self.required_services, timeout=self.execution_timeout)
+        self._widget.groupArguments.setEnabled(False)
+
+    def kill_exec_process(self):
         '''
         Function to send SIGINT to the launch file's process
         '''
-        self.configure_launch_button('red', 'Killing...\nplease wait...')
-        if self.launch_process:
+        self.configure_exec_button('red', 'Killing...\nplease wait...')
+        self._widget.groupArguments.setEnabled(False)
+        if self.exec_process:
             if not self.only_print_cmd:
-                os.killpg(os.getpgid(self.launch_process.pid), signal.SIGINT)
+                os.killpg(os.getpgid(self.exec_process.pid), signal.SIGINT)
                 # wait for the process to complete
-                self.launch_process.wait()
-            self.launch_process = None
-        self.configure_launch_button('orange', 'Down\nclick to\nLaunch')
+                self.exec_process.wait()
+            self.exec_process = None
+        self.configure_exec_button_down()
+        self._widget.groupArguments.setEnabled(True)
 
     def cmd_save_config_clicked(self):
         rospy.loginfo('cmd_save_config_clicked')
@@ -255,7 +358,7 @@ class RqtSimpleLauncher(Plugin):
             yaml_path = yaml_path + '.yaml'
         # create master dictionary
         master_dict = {}
-        master_dict.update({'brief': self._widget.lblBrief.text()})
+        master_dict.update({'brief': self._widget.groupButtons.title()})
         master_dict.update({'launch': self.launch})
         bool_arg_checked = False
         if self._widget.chkBoolParam.isChecked():
